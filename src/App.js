d1a32a01f2df215830009edf91b613cbde96bd6e
lisandroojeda <l.ojedafernandez@gmail.com>
2022-03-30 20:58:09 -0300

    Ejercicio 6 y 7  Fake Api + useEffect + Router

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import NavBar from "./components/navegacion/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
 import Abaut from "./components/pages/Abaut";
 import Catalog from "./components/pages/Catalog";
 import Contact from "./components/pages/Contact";
@@ -11,29 +11,33 @@ import CartWidget from "./components/pages/CartWidget";
 import Sumate from "./components/pages/Sumate";
 import ItemListContainer from "./components/pages/ItemListContainer"
 import ApiExterna from "./components/apis/ApiExterna";
-
+import Footer from "./components/pages/Footer"
+import ItemDetailContainer from "./components/pages/ItemDetailContainer";
 
 function App() {
   const saludo= "hola after"
   return (
     <div className="App">
-      <Router>
+      <BrowserRouter>
         <NavBar />
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/Abaut" element={<Abaut />} />
           <Route path="/Contact" element={<Contact saludo = {saludo}/>} />
-          <Route path="/ItemListContainer" element={<ItemListContainer />} />
+          <Route path="/Producto/:categoryId" element={<ItemListContainer />} />
+          <Route path="/Producto/" element={<ItemListContainer />} />
           <Route path="/Catalog" element={<Catalog />} />
           <Route path="/CartWidget" element={<CartWidget />} />
           <Route path="/Sumate" element={<Sumate />} />
           <Route path="/ApiExterna" element={<ApiExterna />} />
+          <Route path="/ItemDetailContainer/:id" element={<ItemDetailContainer />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
       <header className="App-header">
         <img src={logo} className="" width="25%" alt="logo" />
         <h1 className="mt-4">Pagina en construcción</h1>
       </header>
+      {<Footer/>}
     </div>
   );
 }
---
+++ src/components/pages/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+        aca van las redes sociales
+    </div>
+  )
+}
+
+export default Footer
